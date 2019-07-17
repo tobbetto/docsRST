487df4f30b974da75cc03606dd19216180402b89
Thorvaldur Konradsson <tobbetto@users.noreply.github.com>
2019-07-17 15:47:50 +0200

    Update conf.py

--- conf.py
+++ conf.py
@@ -67,4 +67,4 @@ html_static_path = ['_static']
 #html_theme = 'sphinx_pdj_theme'
 #import sphinx_pdj_theme
 #html_theme_path = [sphinx_pdj_theme.get_html_theme_path()]
-html_theme_path = [sphinx_bootstrap_theme.get_html_theme_path()]
+# html_theme_path = [sphinx_bootstrap_theme.get_html_theme_path()]
