855269093dfbe0aa3930be4a002aaaa6a2c7d118
Thorvaldur Konradsson <tobbetto@users.noreply.github.com>
2019-07-17 13:36:30 +0200

    first bootstrap

--- _build/html/_sources/index.rst.txt
+++ _build/html/_sources/index.rst.txt
@@ -6,15 +6,18 @@
 Welcome to techDocTLPZ's documentation!
 =======================================
 
+.. container:: custom
+
 .. toctree::
    :maxdepth: 1
    :caption: Contents:
-   
+  
    rst_docs/es_es/technical documentation/orders/Manual de usuario
    rst_docs/es_es/functional documentation/appification/funcionalidades_apificacion_1_4
    docs/orderflow_2
    rst_docs/es_es/Welcome/acceso_plataforma_gtu
    rst_docs/plantillas/tobbi
+   rst_docs/plantillas/plantilla_documentacion_word
 
 
 Indices and tables
--- conf.py
+++ conf.py
@@ -55,12 +55,19 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+import sphinx_bootstrap_theme
+# html_theme = 'sphinx_rtd_theme'
+html_theme = 'bootstrap'
+html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+#html_theme = 'sphinx_pdj_theme'
+#import sphinx_pdj_theme
+#html_theme_path = [sphinx_pdj_theme.get_html_theme_path()]
 
 #html_theme = 'sphinx_pdj_theme'
 #import sphinx_pdj_theme
