82055f0b230dd76b2c852a0d4708453de4ee7020
Thorvaldur Konradsson <tobbetto@users.noreply.github.com>
2019-07-17 15:00:45 +0200

    Update conf.py

--- conf.py
+++ conf.py
@@ -13,7 +13,7 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
-
+import sphinx_bootstrap_theme
 
 # -- Project information -----------------------------------------------------
 
@@ -55,9 +55,9 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-# import sphinx_bootstrap_theme
-html_theme = 'sphinx_rtd_theme'
-# html_theme = 'bootstrap'
+
+# html_theme = 'sphinx_rtd_theme'
+html_theme = 'bootstrap'
 html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 
 # Add any paths that contain custom static files (such as style sheets) here,
